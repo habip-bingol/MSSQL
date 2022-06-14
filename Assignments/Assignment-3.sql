e62ed15e9fd751d88492588e6fb28d658b5c3bc0
habip <hbpbngl25@gmail.com>
2022-06-14 20:40:11 +0300

    updated

--- Assignments/Assignment-3.sql
+++ Assignments/Assignment-3.sql
@@ -49,3 +49,26 @@ FROM	(SELECT *,
 		FROM sample1
 		) A
 GROUP BY A.[Adv_Type]
+
+
+-- Alternative 
+
+
+WITH CTE1 AS 
+(
+select Adv_type, 
+	   count(*) total_action
+from #T1 
+group by Adv_type
+), CTE2 AS
+(
+select Adv_type, 
+	   count(*) order_action
+from #T1 
+where Action = 'order'
+group by Adv_type
+
+)
+SELECT CTE1.adv_type, CTE1.total_action, CTE2.order_action, cast(1.0*order_action/total_action as decimal (3,2)) AS conversion_rate
+FROM CTE1, CTE2 
+WHERE CTE1.adv_type = CTE2.adv_type
